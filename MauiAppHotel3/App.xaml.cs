46a6070b02cd2992fab404bf0221c7af632a1c71
Helem Necesia <hellems2necesia_@hotmail.com>
2024-11-17 07:22:50 -0300

    Adicionar recursos ao App

--- MauiAppHotel3/App.xaml.cs
+++ MauiAppHotel3/App.xaml.cs
@@ -1,8 +1,37 @@
-﻿
+﻿using MauiAppHotel3.Models;
+
 namespace MauiAppHotel3
 {
     public partial class App : Application
     {
+        public List<Quarto> list_quartos = new List<Quarto>
+            {
+            new Quarto ()
+            {
+                Descricao = "Suíte Super Luxo",
+                ValorDiariaAdulto = 110.00,
+                ValorDiariaCrianca = 55.00
+            },
+            new Quarto ()
+            {
+                Descricao = "Suíte Luxo",
+                ValorDiariaAdulto = 80.00,
+                ValorDiariaCrianca = 40.00 
+            },
+            new Quarto ()
+            {
+                Descricao = "Suíte Single",
+                ValorDiariaAdulto = 50.00,
+                ValorDiariaCrianca = 25.00
+            },
+            new Quarto ()
+            {
+                Descricao = "Suíte Crise",
+                ValorDiariaAdulto = 25.00,
+                ValorDiariaCrianca = 12.50
+            },
+
+        };
         public App()
         {
             InitializeComponent();
